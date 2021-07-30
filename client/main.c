3d26e23a842e3bd8a19c02df2dd61934d517c2cb
Q <q@qnixsoft.com>
2021-07-30 23:04:02 +0100

    mcp + tty with js

--- client/main.c
+++
@@ -1,9 +0,0 @@
-#include <metal.h>
-#include "mcp.h"
-#include "view.h"
-
-int
-main(int argc, char *argv[], char *envp[])
-{
-	return 0;
-}
--- client/tty.h
+++
@@ -1,7 +0,0 @@
-#ifndef TTY_H
-#define TTY_H
-
-#include <stdlib.h>
-size_t tty_proc(char *out, char *input);
-
-#endif
